41802bab4b24a7b8f54cd892551e2fe8742c5228
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-10-07 20:07:46 +0500

    Create q2.c

---
+++ q2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+  FILE *fp;
+  FILE *fp1;
+  int x;
+  char ch;
+  fp = fopen("file.txt", "r+");
+  fp1 = fopen("file1.txt", "w");
+  if(fp == NULL) {
+    printf("File doesnot exist.\n");
+  }
+  do {
+    ch = fgetc(fp);
+    if(ch >= 48 && ch <= 57) {
+      fputc(ch, fp1);
+    }
+  } while(ch != EOF);
+  fclose(fp);
+  fclose(fp1);
+}
